54e27229ab9e8fb11bda36b0ace9746140a4b58e
oz-mollerstuen <mollerstuen.works@gmail.com>
2022-10-19 15:32:54 -0700

    add branching
    Co-authored-by: Sonny Snyder <sendsonnymail@gmail.com>

--- js/script.js
+++ js/script.js
@@ -2,5 +2,28 @@ window.onload = function() {
   const form = document.querySelector("form");
   form.onsubmit = function(event) {
     event.preventDefault();
-  }
-}
+
+    let response = document.getElementById("animal").value;
+
+    console.log(response);
+
+   
+
+
+    if (response === "badger") {
+      document.getElementById("badger").removeAttribute("class");
+    }
+    else if (response === "lep") {
+      document.querySelector("div#lep").removeAttribute("class");
+    }
+    
+    else if (response === "wheel") {
+      document.querySelector("div#wheel").removeAttribute("class");
+    }
+    else {
+      document.querySelector("div#missed").removeAttribute("class");
+    }
+  
+  
+  };
+};
